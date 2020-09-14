ca61bc8b854ef02743f16c86d0ad8c60230a443c
KaluginaMarina <marin.kalugin@gmail.com>
2020-09-14 21:43:48 +0300

    исправлен арксинус

--- testing/lab1/src/main/kotlin/Function.kt
+++ testing/lab1/src/main/kotlin/Function.kt
@@ -1,22 +1,28 @@
+import java.lang.Math.PI
 import java.lang.Math.abs
 
 class Function {
     companion object {
-        const val PRECISION = 1E-16
+        const val PRECISION = 1E-8
 
         fun arcsin(x: Double): Double? {
             var current = x
             var result = 0.0
             var n = 1
-            if (abs(x) <= 1) {
-                while (current >= PRECISION) {
+            if (x == 1.0) {
+                return PI / 2
+            } else if (x == -1.0) {
+                return -PI / 2
+            } else if (abs(x) < 1) {
+                while (abs(current) >= PRECISION / 10) {
                     result += current
-                    current = -current * x * x / (2 * n++ + 1)
+                    current = current * x * x * (2 * n - 1) * (2 * n - 1) / ((2 * n) * (2 * n + 1))
+                    n++
                 }
             } else {
                 return null
             }
-            return Math.asin(x)
+            return result
         }
     }
 }
--- testing/lab1/src/test/kotlin/FunctionTest.kt
+++ testing/lab1/src/test/kotlin/FunctionTest.kt
@@ -6,46 +6,46 @@ import kotlin.math.asin
 class FunctionTest {
     @Test
     fun testRightBorder(){
-        assertEquals(asin(1.0), Function.arcsin(1.0), "Правая граница не верна")
+        assertEquals(asin(1.0), Function.arcsin(1.0)!!, Function.PRECISION, "Правая граница не верна. Тест: asin(1.0)")
     }
 
     @Test
     fun testLeftBourder(){
-        assertEquals(asin(-1.0), Function.arcsin(-1.0), "Левая граница не верна")
+        assertEquals(asin(-1.0), Function.arcsin(-1.0)!!, Function.PRECISION, "Левая граница не верна. Тест: asin(-1.0)")
     }
 
     @Test
     fun testYIntercept(){
-        assertEquals(asin(0.0), Function.arcsin(0.0), "Пересечение с осью ордина не верно")
+        assertEquals(asin(0.0), Function.arcsin(0.0)!!, Function.PRECISION, "Пересечение с осью ордина не верно. Тест: asin(0.0)")
     }
 
     @Test
     fun testLeftPartOfThePositiveSegment(){
-        assertEquals(asin(0.33), Function.arcsin(0.33), "Левая часть положительного отрезка не верна")
+        assertEquals(asin(0.33), Function.arcsin(0.33)!!,  Function.PRECISION,"Левая часть положительного отрезка не верна. Тест: asin(0.33)")
     }
 
     @Test
     fun testRightPartOfThePositiveSegment() {
-        assertEquals(asin(0.67), Function.arcsin(0.67), "Правая часть положительного отрезка не верна")
+        assertEquals(asin(0.67), Function.arcsin(0.67)!!, Function.PRECISION, "Правая часть положительного отрезка не верна. Тест: asin(0.67)")
     }
 
     @Test
     fun testLeftPartOfTheNegativeSegment() {
-        assertEquals(asin(-0.67), Function.arcsin(-0.67), "Последняя часть положительного отрезка не верна")
+        assertEquals(asin(-0.67), Function.arcsin(-0.67)!!, Function.PRECISION, "Последняя часть положительного отрезка не верна. Тест: asin(-0.67)")
     }
 
     @Test
     fun testRightPartOfTheNegativeSegment() {
-        assertEquals(asin(-0.33), Function.arcsin(-0.33), "Последняя часть положительного отрезка не верна")
+        assertEquals(asin(-0.33), Function.arcsin(-0.33)!!, Function.PRECISION, "Последняя часть положительного отрезка не верна. Тест: asin(-0.33)")
     }
 
     @Test
     fun testOutOfBoundsOnTheRight(){
-        assertNull(Function.arcsin(1.5))
+        assertNull(Function.arcsin(1.5), "Тест на выход за границы справа.")
     }
 
     @Test
     fun testOutOfBoundsOnTheLeft(){
-        assertNull(Function.arcsin(-1.5))
+        assertNull(Function.arcsin(-1.5), "Тест на выход за границы слева.")
     }
 }
