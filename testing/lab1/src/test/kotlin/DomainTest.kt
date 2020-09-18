3ae2fc3fae1de90173184a71f2ce90ce14296bc2
KaluginaMarina <marin.kalugin@gmail.com>
2020-09-19 00:05:56 +0300

    вынесен stdout в переменную и созданы тесты для Бога, Макдака и Географической единицы

--- testing/lab1/src/test/kotlin/DomainTest.kt
+++ testing/lab1/src/test/kotlin/DomainTest.kt
@@ -1,10 +1,31 @@
+import domain.GeographicUnit
+import domain.God
 import domain.TypeOfGeographicUnit
 import domain.Word
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
 
 class DomainTest {
+
+    val standardOut = System.out
+    val outputStreamCaptor = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+
     @Test
     fun testCreatingWorldGeoUnit(){
         val newWorld = Word("Земля", ArrayList(), ArrayList())
@@ -20,6 +41,7 @@ class DomainTest {
         }
     }
 
+    @Test
     fun testCreatingWorldDollar(){
         val newWorld = Word("Земля", ArrayList(), ArrayList())
         newWorld.create()
@@ -27,7 +49,8 @@ class DomainTest {
         assertEquals(true, newWorld.dollar.isExist, "При создании мира доллар должен существовать.")
     }
 
-    fun testCreatingWorldActors(){
+    @Test
+    fun testCreatingActors(){
         val newWorld = Word("Земля", ArrayList(), ArrayList())
         newWorld.create()
         for (actor in newWorld.actors) {
@@ -36,5 +59,59 @@ class DomainTest {
             assert(actor.isExist, {"После создания мира актер должен существовать. Ошибка с актером ${actor.lastName}."})
             assert(actor.countFilms >= 0, {"Количество фильмов должно быть больше или рабно 0. Ошибка с актером ${actor.lastName}."})
         }
+    }
+
+    @Test
+    fun testCreatingGeoUnit(){
+        val newWorld = Word("Земля", ArrayList(), ArrayList())
+        newWorld.create()
+
+        for (geoUnit in newWorld.units) {
+            assertNotNull(geoUnit.name, "Имя у созданного географического объекта должно существовать")
+            assert(geoUnit.size > 0, {"Размер созданного географического объекта должен быть больше 0"})
+            assert(geoUnit.type in TypeOfGeographicUnit.values(), {"Тип географического объекта должен быть из TypeOfGeographicUnit"})
+            assert(geoUnit.isExists, {"Только что созданый географический объект должен существовать"})
+        }
+    }
+
+    @Test
+    fun testCreateMcDonalds() {
+        val newWorld = Word("Земля", ArrayList(), ArrayList())
+        newWorld.create()
+
+        assert(newWorld.mcducks.isExist, {"Только что созданный магазин макдональдс должен существовать"})
+        assert(newWorld.mcducks.hamburgerCount >= 0, {"Число гамбургеров в макдаке не может быть отрицательным"})
+        assert(newWorld.mcducks.countMcdonalds >= 0, {"Колтчество макдаков не может быть мельне 0"})
+        assert(newWorld.mcducks.getSize() > 0, {"Занимаемая площадь макдаков на планете должен быть больше 0"})
+    }
+
+    @Test
+    fun testGodCreatingWorld(){
+        God.createWorld()
+        assertEquals("На первый день Бог создал небо, землю и свет, и отделил свет от тьмы;\n" +
+                "на второй день — создал твердь посреди воды, отделил воду над твердью от воды под твердью, и назвал " +
+                "твердь небом; \nна третий — сушу, моря и растения, \nна четвёртый — светила на тверди небесной, \nна пятый — " +
+                "рыб, пресмыкающихся и птиц. \nНаконец, на шестой — зверей земных, скот, гадов земных и человека.",
+            outputStreamCaptor.toString().trim(),
+            "Сказанное Богом не соответствует тексту в Библии."
+        )
+    }
+
+    @Test
+    fun testGodGeoUnit(){
+        God.createGeoUnit(GeographicUnit("Бали", 10000.0, TypeOfGeographicUnit.COUNTRY), 6)
+        assertEquals("На 6 день создал Бог место под названием Бали, размером 10000.0 км^2",
+            outputStreamCaptor.toString().trim(),
+            "При создании географического объекта Бог говорит что-то не то."
+        )
+    }
+
+    @Test
+    fun testGodDistractionGeoUnit(){
+        God.destructionGeoUnit(GeographicUnit("Бали", 10000.0, TypeOfGeographicUnit.COUNTRY))
+        assertEquals("Место с названием Бали было уничтожено.",
+            outputStreamCaptor.toString().trim(),
+            "При уничтожении географического объекта Бог говорит что-то не то."
+        )
     }
 }
