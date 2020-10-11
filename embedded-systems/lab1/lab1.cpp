518f6887598e1d4f8b6b99a610d7ea09c086b383
KaluginaMarina <marin.kalugin@gmail.com>
2020-10-11 19:40:48 +0300

    [вс] кнопка

--- embedded-systems/lab1/lab1.cpp
+++ embedded-systems/lab1/lab1.cpp
@@ -1,34 +1,76 @@
 #include "hal.h"
 
+
+GPIO_PinState state;
+GPIO_PinState state1;
+
+void checknBTN(){
+    state1 = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
+    if (state1 != state) {
+        state = state1;
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+        HAL_Delay(100);
+        state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
+        while((state != state1)) {
+            state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
+        }
+        state = state1;
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+    }
+}
+
 int umain(){
     int i = 0, delay = 500;
     int leds_num[] = {GPIO_PIN_3,GPIO_PIN_4,GPIO_PIN_5,GPIO_PIN_6,GPIO_PIN_8,GPIO_PIN_9,GPIO_PIN_11,GPIO_PIN_12};
     unsigned int sw_num[] = {GPIO_PIN_4, GPIO_PIN_8, GPIO_PIN_10, GPIO_PIN_12};
 
+    state = HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
+
     while(1){
 
         if(HAL_GPIO_ReadPin(GPIOE, sw_num[1])){
 
+            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+
             for (i = 0; i < 4; i++){
                 HAL_GPIO_WritePin(GPIOD, leds_num[i%4], GPIO_PIN_SET);
                 HAL_GPIO_WritePin(GPIOD, leds_num[i%4 + 4], GPIO_PIN_SET);
+                checknBTN();
                 HAL_Delay(delay);
+                checknBTN();
                 HAL_GPIO_WritePin(GPIOD, leds_num[i%4], GPIO_PIN_RESET);
                 HAL_GPIO_WritePin(GPIOD, leds_num[i%4 + 4], GPIO_PIN_RESET);
+                checknBTN();
                 HAL_Delay(delay);
+                checknBTN();
             }
 
 
             for (i = 2; i >= 0; i--){
                 HAL_GPIO_WritePin(GPIOD, leds_num[i%4], GPIO_PIN_SET);
                 HAL_GPIO_WritePin(GPIOD, leds_num[i%4 + 4], GPIO_PIN_SET);
+                checknBTN();
                 HAL_Delay(delay);
+                checknBTN();
                 HAL_GPIO_WritePin(GPIOD, leds_num[i%4], GPIO_PIN_RESET);
                 HAL_GPIO_WritePin(GPIOD, leds_num[i%4 + 4], GPIO_PIN_RESET);
+                checknBTN();
                 HAL_Delay(delay);
+                checknBTN();
             }
+
         } else {
-            
+           
+            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+            HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+
             for (i = 0; i < 4; ++i) {
                 if (i == 1) {
                     continue;
